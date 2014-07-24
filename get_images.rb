9824e25b1e3c67b6c41ea2f84238b31ddb057779
Ryan Boucher <ryan.boucher@distributedlife.com>
2014-07-24 12:56:26 +0200

    run of raw; skip duplicate entries

--- get_images.rb
+++ get_images.rb
@@ -5,21 +5,21 @@ require 'pandoc-ruby'
 require "unicode_utils/each_grapheme"
 require 'json'
 
-@birds = []
+@animals = []
 
-File.open("birds.json") do |file|
-	@birds = JSON.parse(file.read)
+File.open(ARGV[0]) do |file|
+	@animals = JSON.parse(file.read)
 end
 
-@birds['birds'].each do |bird|
-	puts bird['common_name']
+@animals['animals'].each do |animal|
+	puts animal['common_name']
 
-	next if bird['old_filename'].empty?
-	next if File.exists? bird['new_filename']
+	next if animal['old_filename'].empty?
+	next if File.exists? animal['new_filename']
 
-	data = eat("http:#{bird['old_filename']}")
+	data = eat("http:#{animal['old_filename']}")
 
-	File.open(bird['new_filename'], "w") do |file|
+	File.open(animal['new_filename'], "w") do |file|
 		file.write(data)
 	end
 end
